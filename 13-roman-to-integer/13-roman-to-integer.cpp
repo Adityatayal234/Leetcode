fd8af8f8c1c6ae61df385e6035ccde9073d30e50
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-02-21 23:39:59 +0530

    Time: 24 ms (32.10%), Space: 8.4 MB (15.36%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char,int> m;
-        // m['I'] = 1;
-        // m['V'] = 5;
-        // m['X'] = 10;
-        // m['L'] = 50;
-        // m['C'] = 100;
-        // m['D'] = 500;
-        // m['M'] = 1000;
-        m.insert(pair<char,int>('I',1)); 
-        m.insert(pair<char,int>('V',5)); 
-        m.insert(pair<char,int>('X',10)); 
-        m.insert(pair<char,int>('L',50)); 
-        m.insert(pair<char,int>('C',100)); 
-        m.insert(pair<char,int>('D',500)); 
-        m.insert(pair<char,int>('M',1000)); 
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        // m.insert(pair<char,int>('I',1)); 
+        // m.insert(pair<char,int>('V',5)); 
+        // m.insert(pair<char,int>('X',10)); 
+        // m.insert(pair<char,int>('L',50)); 
+        // m.insert(pair<char,int>('C',100)); 
+        // m.insert(pair<char,int>('D',500)); 
+        // m.insert(pair<char,int>('M',1000)); 
          
         int ans = 0;
         
