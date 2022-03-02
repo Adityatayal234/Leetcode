5d20eed23d67a8a00797bdefc8a265700c250cd5
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-03-02 21:31:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (41.22%) - LeetHub

---
+++ 1134-armstrong-number/1134-armstrong-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isArmstrong(int n) {
+        int num = 0;
+        int original = n;
+        
+        while(n>0){
+            num++;
+            n = n/10; 
+        }
+        cout<<num;
+        n =original;
+        int ans=0;
+        
+        while(n>0){
+            int rem = n%10;
+            ans+=pow(rem,num);
+            n = n/10;
+        }
+        
+        if(ans == original)
+            return true;
+        else
+            return false;
+        
+    }
+};
