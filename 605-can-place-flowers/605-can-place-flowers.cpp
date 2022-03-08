ff791fbb5ff89aac5071818aa4847d6dc7d88a2d
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-03-08 21:44:24 +0530

    Time: 29 ms (48.75%), Space: 20.3 MB (70.71%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -5,13 +5,18 @@ public:
         int count = 0;
         
         for (int i = 0; i < flowerbed.size(); i++) {
+            
             // Check if the current plot is empty.
+            
             if (flowerbed[i] == 0) {
+                
                 // Check if the left and right plots are empty.
                 bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
                 bool emptyRightPlot = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
                 
+                
                 // If both plots are empty, we can plant a flower here.
+                
                 if (emptyLeftPlot && emptyRightPlot) {
                     flowerbed[i] = 1;
                     count++;
