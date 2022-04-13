714d6e03e9ea884b5957b7270f13739d75134237
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-04-13 16:53:46 +0530

    Time: 21 ms (19.16%), Space: 7.2 MB (78.32%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,25 +3,31 @@ public:
     
     bool isAnagram(string s, string t) {
         
-        if(s.length() != t.length())
-            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
-        map<int,int> m;
+        return s==t;
+        
+//         if(s.length() != t.length())
+//             return false;
+        
+//         map<int,int> m;
         
         
-        for(int i=0 ; i<s.length() ; i++){
-            m[s[i]]++;
-        }
+//         for(int i=0 ; i<s.length() ; i++){
+//             cout<<s[i]-'a';
+//             m[s[i]]++;
+//         }
         
-        for(int i=0 ; i<t.length() ; i++){
-            m[t[i]]--;
-        }
+//         for(int i=0 ; i<t.length() ; i++){
+//             m[t[i]]--;
+//         }
         
-        for(int i=0 ; i<s.length() ; i++)
-            if(m[s[i]] != 0)
-                return false;
+//         for(int i=0 ; i<s.length() ; i++)
+//             if(m[s[i]] != 0)
+//                 return false;
         
-        return true;
+//         return true;
         
     }
 };
