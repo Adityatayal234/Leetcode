5c7bb3cf59479cc65e29f85b5971e3a3959713dc
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-05-03 20:36:58 +0530

    Time: 55 ms (25.58%), Space: 26.4 MB (89.65%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,53 +1,28 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int min1=INT_MAX, max1=INT_MIN;
-        bool sorted = true;
-        int sizee = nums.size();
         
-        for(int i=1 ; i<sizee; i++){
-            //cout<<"----Inside 1st loop  "<<nums[i]<<"  "<<nums[i-1]<<endl;
-            if(nums[i] < nums[i-1])
-                sorted = false;
-            if(!sorted){
-              //   cout<<"not sorted loop ";
-                min1 = min(min1,nums[i]);
-               // cout<<min1<<endl;
-            }
-        }
-        sorted = true;
-        //cout<<"--------------------------"<<"\n";
-        for(int i=sizee-2 ; i>=0 ; i--){
-          //  cout<<"Inside 2nd loop  "<<nums[i]<<"  "<<nums[i+1]<<endl;
-            if(nums[i] > nums[i+1])
-                sorted = false;
-            if(!sorted){
-            //    cout<<"not sorted loop  ";
-                max1 = max(max1,nums[i]);
-             //   cout<<max1<<endl;
-            }
+        int end = -1;
+        int max1 = nums[0];
+        
+        for(int i=1 ; i<nums.size() ; i++){
+            if(nums[i] < max1)
+                end = i;
+            else
+                max1 = nums[i];
         }
         
-        int l,r;
+        int start =0;
+        int min1 = nums[nums.size()-1];
         
-        for( l=0 ; l<sizee ; l++){
-            if(nums[l] > min1){
-                break;
-            }
+        for(int i=nums.size()-2 ; i>=0 ; i--){
+            if(nums[i] > min1)
+                start = i;
+            else
+                min1 = nums[i];
         }
         
-        for( r=sizee-1 ; r>=0 ; r--){
-            if(nums[r] < max1){
-                break;
-            }       
-        }
-        
-            
-            
-        cout<<min1<<"     "<<max1<<endl;
-        cout<<l<<" "<<r;
-    
-         return r - l < 0 ? 0 : r - l + 1;
+        return end-start+1;
         
     }
 };
