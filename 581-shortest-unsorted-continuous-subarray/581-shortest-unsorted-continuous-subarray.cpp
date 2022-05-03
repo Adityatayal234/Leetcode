50c1d24c3dfb3531972afdce7671dac1afc0c06b
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-05-03 20:39:10 +0530

    Time: 53 ms (28.43%), Space: 26.5 MB (89.65%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -5,8 +5,10 @@ public:
         int end = -1;
         int max1 = nums[0];
         
-        for(int i=1 ; i<nums.size() ; i++){
-            if(nums[i] < max1)
+      
+        for(int i=1 ; i<nums.size() ; i++){ // loop for l->r
+            
+            if(nums[i] < max1)    // check if nums[i] i.e current element is smaller than the                                        previous element for max
                 end = i;
             else
                 max1 = nums[i];
@@ -15,8 +17,9 @@ public:
         int start =0;
         int min1 = nums[nums.size()-1];
         
-        for(int i=nums.size()-2 ; i>=0 ; i--){
-            if(nums[i] > min1)
+        for(int i=nums.size()-2 ; i>=0 ; i--){ // loop for r->l
+            
+            if(nums[i] > min1)  // check if nums[i] i.e current element is larger than the                                        previous right element for max
                 start = i;
             else
                 min1 = nums[i];
