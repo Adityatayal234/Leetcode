afcbdb921819348a5cf02a6601e71a4ff31fd6a9
Adityatayal234 <84038935+Adityatayal234@users.noreply.github.com>
2022-05-13 16:49:32 +0530

    Time: 92 ms (17.25%), Space: 33.1 MB (5.01%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool valid(string s, int left, int right, int count){
+    bool isValid(string s , int left , int right , int count){
         
-        if(count >1)
+        if(count>1)
             return false;
+        
         while(left < right){
-            
             if(s[left] == s[right]){
                 left++;
                 right--;
             }
             else{
-                return valid(s,left+1,right,count+1) || valid(s,left,right-1,count+1);
-            }  
+                return isValid(s,left+1,right,count+1) || isValid(s,left,right-1,count+1);
+            }
         }
+        
         return true;
     }
     
     bool validPalindrome(string s) {
-        int left=0, right = s.size()-1;
-        return valid(s,left,right,0);
+        int left = 0 , right = s.size()-1 ;
+        return isValid(s,left,right,0);
     }
 };
